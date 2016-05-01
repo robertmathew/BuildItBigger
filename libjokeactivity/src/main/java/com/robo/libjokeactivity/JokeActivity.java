896a947ef2e6bea48d953faecab8e38549de4b7b
robo <robu1691@gmail.com>
2016-05-01 12:42:24 +0530

    Fixed backend and added some styling

--- libjokeactivity/src/main/java/com/robo/libjokeactivity/JokeActivity.java
+++ libjokeactivity/src/main/java/com/robo/libjokeactivity/JokeActivity.java
@@ -2,6 +2,7 @@ package com.robo.libjokeactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
@@ -11,8 +12,12 @@ public class JokeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         String joke = getIntent().getStringExtra("joke");
         TextView tv = (TextView) findViewById(R.id.tvJoke);
+        assert tv != null;
         tv.setText(joke);
     }
 }
