9bb11147053cedcaef741da5bcbb4c0cbda83a78
robo <robu1691@gmail.com>
2016-05-06 10:27:58 +0530

    Add Interstitial Ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.robo.libjokeactivity.JokeActivity;
 
 
@@ -21,6 +23,7 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
 
     private String strJoke;
     private Button mButtonJoke;
+    InterstitialAd mInterstitialAd;
 
     public MainActivityFragment() {
     }
@@ -34,22 +37,48 @@ public class MainActivityFragment extends Fragment implements View.OnClickListen
         mButtonJoke.setOnClickListener(this);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
 
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+
         new GetGcmJoke().execute();
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                tellJoke();
+            }
+        });
+
+        requestNewInterstitial();
 
         return root;
     }
 
     @Override
     public void onClick(View v) {
-        //Toast.makeText(this, strJoke, Toast.LENGTH_SHORT).show();
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            tellJoke();
+        }
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+
+    //Intent to library showing joke
+    private void tellJoke() {
         Intent i = new Intent(getActivity(), JokeActivity.class);
         i.putExtra("joke", strJoke);
         startActivity(i);
